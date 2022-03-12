129e5e67f58c6dfe4bd7ba6cb00de6cb187fc87c
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-11 20:21:46 -0500

    Game Winner Changed the innerhtml of the title to reflect who won based on the dice.

--- index.js
+++ index.js
@@ -15,4 +15,12 @@ var diceImage2 = "images/dice" + randomNumber2 + ".png";
 
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", diceImage2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+} else if(randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+} else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
     
