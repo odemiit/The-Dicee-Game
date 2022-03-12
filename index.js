1df22668283c95299abc2352ef88f93a71d1d32c
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-11 20:17:13 -0500

    Change the Image changed the image of the dice with respect to the number generated.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var randomNumber1 = Math.random();
 randomNumber1 = (randomNumber1 * 6) + 1;
 randomNumber1 = Math.floor(randomNumber1);
+
+var diceImage1 = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", diceImage1);
+
+var randomNumber2 = Math.random();
+randomNumber2 = (randomNumber2 * 6) + 1;
+randomNumber2 = Math.floor(randomNumber2);
+
+var diceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", diceImage2);
+    
